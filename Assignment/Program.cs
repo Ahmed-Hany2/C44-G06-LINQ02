59529b0c07212642cd1246e38f27b4d99c7502c6
Ahmed-Hany2 <ahmedhanypro2@gmail.com>
2025-11-28 15:42:21 +0200

    Part05- Question 3

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -233,6 +233,12 @@ namespace Assignment
             products.GroupBy(p => p.Category)
             .Where(g => g.Any(p => p.UnitsInStock == 0));
 
+            // 3. Return a grouped a list of products only for categories that have all of their products in stock.
+            var allInStock =
+                products.GroupBy(p => p.Category)
+                        .Where(g => g.All(p => p.UnitsInStock > 0));
+
+
             #endregion
         }
     }
