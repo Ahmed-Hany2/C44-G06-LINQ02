64a30d5a8bed5aaac6dbecee52e209d41761210a
Ahmed-Hany2 <ahmedhanypro2@gmail.com>
2025-11-28 14:28:31 +0200

    Part01-Question 1

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-           
+            #region LINQ - Aggregate Operators
+
+            // 1. Uses Count to get the number of odd numbers in the array
+            int[] Arr = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+
+            int oddCount = Arr.Count(n => n % 2 != 0);
+
+
+            #endregion
         }
     }
 }
